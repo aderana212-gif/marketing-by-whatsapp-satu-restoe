b7ccf8dbd70d1e98b8bb1b164599aca53bab0c46
aderana212-gif <aderana212@gmail.com>
2026-09-04 15:40:48 +0700

    Add verified active Subang prospects

--- app/src/main/assets/category_data_extra.js
+++ app/src/main/assets/category_data_extra.js
@@ -4,28 +4,35 @@
 if (typeof CATEGORY_DATA === 'object') {
   CATEGORY_DATA.travel.push(
     ['Banagara Tour Travel','Ciamis','0851-1939-5590','A'],
-    ['Hasby Tour & Travel','Ciamis','0857-8049-7907','A']
+    ['Hasby Tour & Travel','Ciamis','0857-8049-7907','A'],
+    ['BRIDGE HOLIDAY TOUR & TRAVEL','Subang','0818-0910-4875','A'],
+    ['M-Trans Travel Subang','Subang','0857-2009-8262','A']
   );
   CATEGORY_DATA.bus.push(
     ['PT Andalas Galuh Pradja','Bandung/Ciamis','0813-2373-2732','A'],
     ['PT Andalas Galuh Pradja','Bandung/Ciamis','0853-5323-8080','A'],
     ['Agen Bus Budiman Banjar','Kota Banjar','0853-5395-4191','B'],
     ['Agen Bus Budiman Ciamis','Ciamis','0813-2135-5394','B'],
-    ['William Trans','Tangerang','0851-3666-1717','B']
+    ['William Trans','Tangerang','0851-3666-1717','B'],
+    ['CV CBK Tour & Travel / Sewa Bus Pariwisata Subang','Subang','0859-3529-6096','A']
   );
   CATEGORY_DATA.eo.push(
     ['Arjuna Event','Depok / melayani Tasik-Ciamis-Cilacap','0859-3031-9671','A'],
-    ['Arjuna Event','Depok / melayani Tasik-Ciamis-Cilacap','0858-8049-6494','A']
+    ['Arjuna Event','Depok / melayani Tasik-Ciamis-Cilacap','0858-8049-6494','A'],
+    ['Semesta Sinergy','Subang/Ciater','0878-2357-6522','A'],
+    ['UEO Adventure','Subang','0852-1025-2900','A']
   );
   CATEGORY_DATA.corp.push(
     ['SMK Negeri 3 Bandung','Bandung','0811-2398-333','A'],
     ['SMK Pasundan 1 Bandung','Bandung','0858-4664-9000','A'],
     ['SMK Medikacom Bandung','Bandung','0815-8533-8606','A'],
     ['MIN 1 Kota Bandung','Bandung','0877-2417-4111','B'],
-    ['MA Al-Huda Pameungpeuk','Kab. Bandung','0895-3899-2588','B']
+    ['MA Al-Huda Pameungpeuk','Kab. Bandung','0895-3899-2588','B'],
+    ['SMK Pariwisata Ciasem','Subang','0831-3667-8480','A']
   );
   CATEGORY_DATA.wedding.push(
     ['Ahli Wedding Organizer & Catering Ciamis','Ciamis','0821-2087-6664','B'],
-    ['Zen Wedding Planner Purwokerto','Purwokerto','0822-2100-9840','A']
+    ['Zen Wedding Planner Purwokerto','Purwokerto','0822-2100-9840','A'],
+    ['LINTANG WEDDING ORGANIZER','Subang','0821-1551-8914','A']
   );
 }
